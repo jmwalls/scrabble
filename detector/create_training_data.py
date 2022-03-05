21a5145af22f397b2711528af7ffb353df88eab5
jeff <jmwalls@umich.edu>
2022-03-05 07:59:47 -0800

    creating labelled training data

--- detector/create_training_data.py
+++ detector/create_training_data.py
@@ -22,11 +22,13 @@ import ontology
 def read_board_labels(*, path):
     """XXX
     """
-    df = pd.read_csv(path, header=None)
+    df = pd.read_csv(path, header=None, sep=' ')
     assert df.shape == (15, 15)
 
     # Replace board '.' with 'other' class key.
+    # Replace board '?' with 'blank' class key.
     df[df == '.'] = 'other'
+    df[df == '?'] = 'blank'
 
     return [i for i in map(lambda k: ontology.LABELS_TO_CLASSES[k], df.to_numpy().flatten())]
 
@@ -35,14 +37,17 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--canonical', required=True,
                         help='path to serialized canonical board')
-    parser.add_argument('--dir', required=True, help='path to image directory')
+    parser.add_argument('--indir', required=True, help='path to image directory')
     parser.add_argument('--labels', required=True, help='path to board label csv')
+    parser.add_argument('--outdir', required=True, help='path to output label')
     args = parser.parse_args()
 
     labels = read_board_labels(path=args.labels)
     board = CanonicalBoard.create_from_disk(path=args.canonical)
 
-    for p in glob.glob(os.path.join(args.dir, '*.png')):
+    os.makedirs(args.outdir, exist_ok=True)
+
+    for p in glob.glob(os.path.join(args.indir, '*.png')):
         logging.info(f'partitioning {p}')
         tiles = board.partition(image_path=p)
         assert len(tiles) == len(labels)
@@ -51,8 +56,9 @@ def main():
         records = [{'label': labels[i],
                     'tile': t.flatten()} for i, t in enumerate(tiles)]
         df = pd.DataFrame.from_records(records)
-        # XXX out file path...
-        df.to_pickle('foo.pkl')
+
+        pout = f'data_frame.{os.path.splitext(os.path.basename(p))[0]}.pkl'
+        df.to_pickle(os.path.join(args.outdir, pout))
 
 
 if __name__ == '__main__':
